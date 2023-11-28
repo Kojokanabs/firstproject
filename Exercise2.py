95e98329983c63e01ba37448f9e0e29b227bc77f
Enoch K.Ali <Kojokanabs@gmail.com>
2023-11-28 12:43:25 +0000

    This is a program to determine who will pay for the bill among three friends

---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+print("String Manipulation Exercise")
+print('String Concatenation is done with "+" sign eg. print("Hello" + "Jenny")')
+print("New line can be created with a backlash and n")
+
+name = input("What's your name: ")
+input("How old are you: ")
+#input()
+print('"Hello" name')
