7a2fac590073c96e2aeb14faefc2f2d9eec5415d
Enoch K.Ali <Kojokanabs@gmail.com>
2024-01-09 23:16:09 +0000

    This is to determine the number of cans needed to paint and area

---
+++ Paint_calculation.py
@@ -0,0 +1,12 @@
+import math
+
+def paint_calculation(height, width, cover):
+    area = height * width
+    no_of_cans = area/cover
+    no_of_cans = math.ceil(area/cover)
+    print(f"You will need {no_of_cans} cans of paints.")
+h = int(input("Enter the height of the wall:\n"))
+w = int(input("Enter the width of the wall:\n"))
+coverage = 7
+
+paint_calculation(width=w, height=h, cover=coverage)
